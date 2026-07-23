2f13fd07fb4650063a633cd0f45595a6be76f395
GuDeLike <2048627959@qq.com>
2026-07-23 22:42:46 +0800

    我创建了新的功能

---
+++ new_result.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+#define int long long
+#define endl '\n'
+
+using namespace std;
+
+signed main(){
+    int a,b;
+    cin>>a>>b;
+    cout<<a+b<<endl;
+    
+    return 0;
+}
